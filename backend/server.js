f8fc6a8f0c1586a0a8db335a563474b297a43205
Cindylov <cynthiaujunwa4@gmail.com>
2024-10-11 13:58:57 +0100

    Fix static file serving path

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,10 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/activities', activityRoutes);  
 app.use('/api/goals', goalsRouter);
 
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
 
